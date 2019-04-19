055627e9e119a70bef37fb92d50ea6e8443117bf
Oleh SAMOILENKO <osamoile@e1r10p10.unit.ua>
2019-04-19 17:42:07 +0300

    t_bool

--- includes/libft.h
+++ includes/libft.h
@@ -24,6 +24,12 @@ typedef struct		s_list
 	struct s_list	*next;
 }					t_list;
 
+typedef enum		e_bool
+{
+	false,
+	true
+}					t_bool;
+
 void				*ft_memset(void *b, int c, size_t len);
 void				ft_bzero(void *s, size_t n);
 void				*ft_memcpy(void *dst, const void *src, size_t n);
